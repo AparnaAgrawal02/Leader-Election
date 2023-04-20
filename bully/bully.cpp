468ff9cdeb163d5fe36b8101762078a0d7856f2d
Dhruv Hirpara <dhruvhirpara555@gmail.com>
2023-04-20 19:18:06 +0530

    added nummessage analysis

--- bully/bully.cpp
+++ bully/bully.cpp
@@ -1,11 +1,10 @@
-#include<mpi.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <bits/stdc++.h>
 
 using namespace std;
-
 
 // implementation of the bully leader election algorithm
 
@@ -34,170 +33,186 @@ int main(int argc, char *argv[])
     int alive = 1;
     int change = 0;
     // int initiator = 0;
+    int num_recv = 0;
+    int num_sent = 0;
 
-    while (1)
+    // while (1)
+    // {
+    if (rank == simulator)
     {
-        if(rank == simulator)
+        // printf("Enter the array of size %d with index to be dead as 0 and alive 1: ", size-1);
+        cout << "simulator" << endl;
+        cout << "Enter the array of size " << size - 1 << " with index to be dead as 0 and alive 1: " << endl;
+        int arr[size - 1];
+        for (int i = 1; i < size; i++)
         {
-            // printf("Enter the array of size %d with index to be dead as 0 and alive 1: ", size-1);
-            cout << "simulator" << endl;
-            cout << "Enter the array of size " << size-1 << " with index to be dead as 0 and alive 1: " << endl;
-            int arr[size-1];
-            for(int i=1; i<size; i++)
-            {
-                // scanf("%d", &arr[i-1]);
-                cin >> arr[i-1];
-            }
-
-            for (int i = 1; i < size; i++)
-            {
-                if(arr[i-1] == 0)
-                {
-                    int msg = DIE;
-                    MPI_Send(&msg, 1, MPI_INT, i, KILL, MPI_COMM_WORLD);
-                }
-                else
-                {
-                    int msg = ALIVE;
-                    MPI_Send(&msg, 1, MPI_INT, i, ALIVE, MPI_COMM_WORLD);
-                }
-            }
-
-            // int init_proc;
-
-            // printf("Enter the process to start the election: ");
-            // scanf("%d", &init_proc);
-            // // send election message to the process
-            // int msg = ELECTION;
-            // MPI_Send(&msg, 1, MPI_INT, init_proc, ELECTION, MPI_COMM_WORLD);
-            // // MPI_Recv(&msg, 1, MPI_INT, init_proc, OK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        else {
-            cout << "Rank " << rank << " is alive" << endl;
-            // cout << "simulator" << simulator << endl;
-            int msg;
-            MPI_Status status;
-            MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-            if(msg == DIE)
-            {
-                change = 1 ? alive == 1 : 0;
-                alive = 0;
-            }
-            if(msg == ALIVE)
-            {
-                change = 1 ? alive == 0 : 0;
-                alive = 1;
-            }
+            // scanf("%d", &arr[i-1]);
+            cin >> arr[i - 1];
         }
 
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        if(rank == simulator)
+        for (int i = 1; i < size; i++)
         {
-            int initiator;
-            // printf("Enter the process to start the election: ");
-            cout << "Enter the process to start the election: ";
-            // scanf("%d", &initiator);
-            cin >> initiator;
-            // send election message to the process
-            int msg = ELECTION;
-            MPI_Send(&msg, 1, MPI_INT, initiator, ELECTION, MPI_COMM_WORLD);
-            // cout << "Rank " << rank << " sent election message to " << initiator << endl;
-            MPI_Recv(&msg, 1, MPI_INT, initiator, OK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        }
-        // MPI_Bcast(&initiator, 1, MPI_INT, simulator, MPI_COMM_WORLD);
-
-        // MPI_Barrier(MPI_COMM_WORLD);
-
-        // listen for
-
-        if(rank != simulator)
-        {
-            // listen for message
-            int election = 0;
-            MPI_Status status;
-            int msg;
-            int leader_elctd = 0;
-
-            while (leader_elctd == 0 && alive == 1)
+            if (arr[i - 1] == 0)
             {
-                MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                cout << "Rank " << rank << " received message from " << status.MPI_SOURCE << " with tag " << status.MPI_TAG << endl;
-                if (status.MPI_TAG == ELECTION)
-                {
-                    // send ok message to the sender
-
-                    int reply = OK;
-                    MPI_Send(&reply, 1, MPI_INT, status.MPI_SOURCE, OK, MPI_COMM_WORLD);
-                    // send election message to all higher ranked processes
-                    if (election == 0)
-                    {
-                        election = 1;
-
-                        for (int i = rank + 1; i < size; i++)
-                        {
-                            MPI_Send(&msg, 1, MPI_INT, i, ELECTION, MPI_COMM_WORLD);
-                        }
-                        // listen for ok messages for 0.5 seconds and then send coordinator message
-                        int ok_count = 0;
-                        int start_time = time(NULL);
-                        MPI_Request request;
-                        MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-                        while (time(NULL) - start_time < 2)
-                        {
-                            int flag;
-                            MPI_Test(&request, &flag, &status);
-                            if (flag)
-                            {
-                                cout << "Rank " << rank << " received message from " << status.MPI_SOURCE << " with tag " << status.MPI_TAG << endl;
-                                if (status.MPI_TAG == OK)
-                                {
-                                    // cout << "Rank " << rank << " received OK from " << status.MPI_SOURCE << endl;
-                                    ok_count++;
-                                }
-                                else if (status.MPI_TAG == ELECTION)
-                                {
-                                    int reply = OK;
-                                    MPI_Send(&reply, 1, MPI_INT, status.MPI_SOURCE, OK, MPI_COMM_WORLD);
-                                }
-                                MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-                                // MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, OK, MPI_COMM_WORLD, &request);                        }
-                            }
-                        }
-                        MPI_Cancel(&request);
-                        if (ok_count == 0)
-                        {
-                            // send coordinator message
-                            int reply = COORDINATOR;
-                            for (int i = rank - 1; i >= 0; i--)
-                            {
-                                MPI_Send(&reply, 1, MPI_INT, i, COORDINATOR, MPI_COMM_WORLD);
-                            }
-                            leader = rank;
-                            leader_elctd = 1;
-                            // printf("Leader elected: %d by rank %d ", leader, rank);
-                            cout << "Leader elected: " << leader << " by rank " << rank << endl;
-
-                        }
-                    }
-
-
-
-                }
-                else if (status.MPI_TAG == COORDINATOR)
-                {
-                    leader = status.MPI_SOURCE;
-                    leader_elctd = 1;
-                    // printf("Leader elected: %d by rank %d ", leader, rank);
-                    cout << "Leader elected: " << leader << " by rank " << rank << endl;
-                }
+                int msg = DIE;
+                MPI_Send(&msg, 1, MPI_INT, i, KILL, MPI_COMM_WORLD);
+            }
+            else
+            {
+                int msg = ALIVE;
+                MPI_Send(&msg, 1, MPI_INT, i, ALIVE, MPI_COMM_WORLD);
             }
         }
 
-        MPI_Barrier(MPI_COMM_WORLD);
+        // int init_proc;
+
+        // printf("Enter the process to start the election: ");
+        // scanf("%d", &init_proc);
+        // // send election message to the process
+        // int msg = ELECTION;
+        // MPI_Send(&msg, 1, MPI_INT, init_proc, ELECTION, MPI_COMM_WORLD);
+        // // MPI_Recv(&msg, 1, MPI_INT, init_proc, OK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    else
+    {
+        cout << "Rank " << rank << " is alive" << endl;
+        // cout << "simulator" << simulator << endl;
+        int msg;
+        MPI_Status status;
+        MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if (msg == DIE)
+        {
+            change = 1 ? alive == 1 : 0;
+            alive = 0;
+        }
+        if (msg == ALIVE)
+        {
+            change = 1 ? alive == 0 : 0;
+            alive = 1;
+        }
     }
 
-}
+    MPI_Barrier(MPI_COMM_WORLD);
 
+    if (rank == simulator)
+    {
+        int initiator;
+        // printf("Enter the process to start the election: ");
+        cout << "Enter the process to start the election: ";
+        // scanf("%d", &initiator);
+        cin >> initiator;
+        // send election message to the process
+        int msg = ELECTION;
+        MPI_Send(&msg, 1, MPI_INT, initiator, ELECTION, MPI_COMM_WORLD);
+        // cout << "Rank " << rank << " sent election message to " << initiator << endl;
+        MPI_Recv(&msg, 1, MPI_INT, initiator, OK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    // MPI_Bcast(&initiator, 1, MPI_INT, simulator, MPI_COMM_WORLD);
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+
+    // listen for
+
+    if (rank != simulator)
+    {
+        // listen for message
+        int election = 0;
+        MPI_Status status;
+        int msg;
+        int leader_elctd = 0;
+
+        while (leader_elctd == 0 && alive == 1)
+        {
+            MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            // cout << "Rank " << rank << " received message from " << status.MPI_SOURCE << " with tag " << status.MPI_TAG << endl;
+            if (status.MPI_SOURCE != simulator)
+            {
+                num_recv++;
+            }
+            if (status.MPI_TAG == ELECTION)
+            {
+                // send ok message to the sender
+
+                int reply = OK;
+                MPI_Send(&reply, 1, MPI_INT, status.MPI_SOURCE, OK, MPI_COMM_WORLD);
+                num_sent++;
+                // send election message to all higher ranked processes
+                if (election == 0)
+                {
+                    election = 1;
+
+                    for (int i = rank + 1; i < size; i++)
+                    {
+                        MPI_Send(&msg, 1, MPI_INT, i, ELECTION, MPI_COMM_WORLD);
+                        num_sent++;
+                    }
+                    // listen for ok messages for 0.5 seconds and then send coordinator message
+                    int ok_count = 0;
+                    int start_time = time(NULL);
+                    MPI_Request request;
+                    MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+                    while (time(NULL) - start_time < 2)
+                    {
+                        int flag;
+                        MPI_Test(&request, &flag, &status);
+                        if (flag)
+                        {
+                            // cout << "Rank " << rank << " received message from " << status.MPI_SOURCE << " with tag " << status.MPI_TAG << endl;
+                            num_recv++;
+                            if (status.MPI_TAG == OK)
+                            {
+                                // cout << "Rank " << rank << " received OK from " << status.MPI_SOURCE << endl;
+                                ok_count++;
+                            }
+                            else if (status.MPI_TAG == ELECTION)
+                            {
+                                int reply = OK;
+                                MPI_Send(&reply, 1, MPI_INT, status.MPI_SOURCE, OK, MPI_COMM_WORLD);
+                                num_sent++;
+                            }
+                            MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+                            // MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, OK, MPI_COMM_WORLD, &request);                        }
+                        }
+                    }
+                    MPI_Cancel(&request);
+                    if (ok_count == 0)
+                    {
+                        // send coordinator message
+                        int reply = COORDINATOR;
+                        for (int i = rank - 1; i >= 0; i--)
+                        {
+                            MPI_Send(&reply, 1, MPI_INT, i, COORDINATOR, MPI_COMM_WORLD);
+                            num_sent++;
+                        }
+                        leader = rank;
+                        leader_elctd = 1;
+                        // printf("Leader elected: %d by rank %d ", leader, rank);
+                        cout << "Leader elected: " << leader << " by rank " << rank << endl;
+                    }
+                }
+            }
+            else if (status.MPI_TAG == COORDINATOR)
+            {
+                leader = status.MPI_SOURCE;
+                leader_elctd = 1;
+                // printf("Leader elected: %d by rank %d ", leader, rank);
+                cout << "Leader elected: " << leader << " by rank " << rank << endl;
+            }
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    // }
+    int total_sent, total_recv;
+    MPI_Reduce(&num_sent, &total_sent, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&num_recv, &total_recv, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == simulator)
+    {
+        cout << "Total messages sent: " << total_sent << endl;
+        cout << "Total messages received: " << total_recv << endl;
+    }
+
+    MPI_Finalize();
+}
--- echo_with_extinction/echo_with_extinction.cpp
+++ echo_with_extinction/echo_with_extinction.cpp
@@ -21,6 +21,8 @@ int main(int argc,char *argv[])
     int simulator = 0;
     int num_neighbours = 0;
     int *neighbours;
+    int num_recv = 0;
+    int num_sent = 0;
     if (rank == simulator){
         cout << "Enter the graph" << endl;
         vector<vector<int>> graph(size);
@@ -75,16 +77,17 @@ int main(int argc,char *argv[])
     }
 
     MPI_Bcast(&initia,size-1,MPI_INT,simulator,MPI_COMM_WORLD);
-    if (rank ==simulator){
-        MPI_Finalize();
-        return 0;
-    }
+    // if (rank ==simulator){
+    //     MPI_Finalize();
+    //     return 0;
+    // }
     if (initia[rank-1] == 1)
     {
         caw = rank;
         int msg = caw;
         for (int i = 0; i < num_neighbours; i++){
             MPI_Send(&msg,1,MPI_INT,neighbours[i],token,MPI_COMM_WORLD);
+            num_sent++;
         }
     }
     while (lrec < num_neighbours)
@@ -92,11 +95,13 @@ int main(int argc,char *argv[])
         int msg;
         MPI_Status status;
         MPI_Recv(&msg,1,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+        num_recv++;
         if (status.MPI_TAG == leader){
             if (lrec == 0){
                 for (int i =0;i < num_neighbours;i++)
                 {
                     MPI_Send(&msg,1,MPI_INT,neighbours[i],leader,MPI_COMM_WORLD);
+                    num_sent++;
                 }
             }
             lrec++;
@@ -110,6 +115,7 @@ int main(int argc,char *argv[])
                 for (int i = 0; i < num_neighbours; i++){
                     if (neighbours[i] != father){
                         MPI_Send(&msg,1,MPI_INT,neighbours[i],token,MPI_COMM_WORLD);
+                        num_sent++;
                     }
                 }
             }
@@ -120,22 +126,36 @@ int main(int argc,char *argv[])
                         int msg = rank;
                         for (int i = 0; i < num_neighbours; i++){
                             MPI_Send(&msg,1,MPI_INT,neighbours[i],leader,MPI_COMM_WORLD);
+                            num_sent++;
                         }
                     }
                     else{
                         MPI_Send(&caw,1,MPI_INT,father,token,MPI_COMM_WORLD);
+                        num_sent++;
                     }
                 }
             }
         }
     }
-    if (winner == rank){
-        s = "leader";
+    // if (winner == rank ){
+    //     s = "leader";
+    // }
+    // else{
+    //     s = "not leader";
+    // }
+
+
+    int total_sent = 0;
+    int total_recv = 0;
+    MPI_Reduce(&num_sent,&total_sent,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+    MPI_Reduce(&num_recv,&total_recv,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+    if (rank == simulator){
+        cout << "Total number of messages sent: " << total_sent << endl;
+        cout << "Total number of messages received: " << total_recv << endl;
     }
-    else{
-        s = "not leader";
+    else {
+        cout << "Leader elected by process " << rank << " is " << winner << endl;
     }
-    cout << "Process " << rank << " is " << s << endl;
     MPI_Finalize();
     return 0;
 
